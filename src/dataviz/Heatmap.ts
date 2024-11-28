c238beb599e2164cc32a39721571109004a1de73
Ryan Sukale <rysukale@gmail.com>
2024-11-27 23:39:40 -0500

    Refactor to create a renderCells function

--- src/dataviz/Heatmap.ts
+++ src/dataviz/Heatmap.ts
@@ -59,8 +59,40 @@ export default class Heatmap<DatumType> {
     this.svg?.remove();
   }
 
+  renderCells({
+    container,
+    cellWidth,
+    cellHeight,
+    cellX,
+    cellY,
+  }: {
+    container: D3Selection["G"];
+    cellWidth: number;
+    cellHeight: number;
+    cellX: (d: DatumType, i: number) => number;
+    cellY: (d: DatumType, i: number) => number;
+  }) {
+    const { cellSpacing } = this;
+
+    const gridGroup = container.selectAll("rect").data(this.data);
+    const gridGroupRects = gridGroup.join("rect");
+
+    setAttrs(
+      {
+        ...this.getCellAttributes(),
+        width: cellWidth - cellSpacing,
+        height: cellHeight - cellSpacing,
+        x: cellX,
+        y: cellY,
+      },
+      gridGroupRects
+    );
+
+    return gridGroup;
+  }
+
   renderGrid(svg: D3Selection["SVG"], width: number, height: number) {
-    const { data, rows, direction, cellSpacing } = this;
+    const { data, rows, direction } = this;
     const cols = data.length / rows;
     const cellHeight = height / rows;
     const cellWidth = width / cols;
@@ -80,22 +112,16 @@ export default class Heatmap<DatumType> {
           cellHeight * Math.floor(index / cols);
     }
 
-    const gridGroup = svg.append("g").selectAll("rect").data(data);
+    const cellsGroup = svg.append("g");
+    this.renderCells({
+      container: cellsGroup,
+      cellWidth,
+      cellHeight,
+      cellX,
+      cellY,
+    });
 
-    const gridGroupRects = gridGroup.join("rect");
-
-    setAttrs(
-      {
-        ...this.getCellAttributes(),
-        width: cellWidth - cellSpacing,
-        height: cellHeight - cellSpacing,
-        x: cellX,
-        y: cellY,
-      },
-      gridGroupRects
-    );
-
-    return gridGroup;
+    return cellsGroup;
   }
 
   render() {
